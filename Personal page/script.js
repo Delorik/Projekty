87a4e8eb2a3750c6b2b12a5272f30365c5c16b4a
Delorik <102295383+Delorik@users.noreply.github.com>
2022-05-29 11:04:08 +0200

    Update script.js

--- Personal page/script.js
+++ Personal page/script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile_menu');
 const menuLinks = document.querySelector('.nav_links_container');
+const navLogo = document.querySelector('.nav_logo')
 
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
@@ -8,6 +9,27 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if (window.innerWidth <= 1100 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+
+
+
+
+
+
+
+
+
+//Phone and mail
 
 var i = 0;
 var txt = 'grubnerp@email.cz';
